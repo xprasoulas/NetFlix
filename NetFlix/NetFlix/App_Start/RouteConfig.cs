01f9dad6922667ccb49616ca00e2206a69e243ea
Charis <xprasoulas@gmail.com>
2019-01-17 20:48:01 +0200

    Change Routing to use it as the action welcome /hellonetflix/welcome/name/1

--- NetFlix/NetFlix/App_Start/RouteConfig.cs
+++ NetFlix/NetFlix/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace NetFlix
@@ -12,6 +8,11 @@ namespace NetFlix
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
 
             routes.MapRoute(
                 name: "Default",
