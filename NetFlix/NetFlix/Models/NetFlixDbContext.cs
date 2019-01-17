378459786259a832acd1848010d79696717873f4
Charis <xprasoulas@gmail.com>
2019-01-17 22:11:50 +0200

    Create Class NetFlixDbContext with properties DbSet collection

---
+++ NetFlix/NetFlix/Models/NetFlixDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;//
+using System.Linq;
+using System.Web;
+
+namespace NetFlix.Models
+{
+    public class NetFlixDbContext : DbContext   
+    {
+        public DbSet<Serie> Series { get; set; } //DbSet is a collection 
+    }
+}
