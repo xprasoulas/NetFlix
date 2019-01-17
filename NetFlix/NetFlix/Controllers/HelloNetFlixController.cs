6601bc47f3c11393c578718402e44798dc4a4f3b
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-17 16:01:25 +0200

    Create Empty NetFlixController

---
+++ NetFlix/NetFlix/Controllers/HelloNetFlixController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetFlix.Controllers
+{
+    public class HelloNetFlixController : Controller
+    {
+        // GET: HelloNetFlix
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
