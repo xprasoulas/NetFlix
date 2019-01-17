823da5f31f9afe89e42287fa2788416a41444ace
Charis <xprasoulas@gmail.com>
2019-01-17 22:07:19 +0200

    Add/Fill Class Serie

---
+++ NetFlix/NetFlix/Models/Serie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetFlix.Models
+{
+    public class Serie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime RealeseData { get; set; }
+        public string Genre { get; set; }
+        public int Seasons { get; set; }
+        public string Rating { get; set; }
+    }
+}
