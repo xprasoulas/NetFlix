e58d25f48f177c59892f784359dd81a590e7b97d
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-20 12:19:49 +0200

    Insert Datetime Annotation

--- NetFlix/NetFlix/Models/Serie.cs
+++ NetFlix/NetFlix/Models/Serie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NetFlix.Models
 {
@@ -6,6 +7,10 @@ namespace NetFlix.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        
+        [Display(Name ="Realesed Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] //bring time that was edit with
         public DateTime RealeseData { get; set; }
         public string Genre { get; set; }
         public int Seasons { get; set; }
