a3a3a64701fdab0e4e720d7db7b47b99030c79d7
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-21 21:05:49 +0200

    add-migration AddColumnPrice

---
+++ NetFlix/NetFlix/Migrations/201901211905123_AddColumnPrice.cs
@@ -0,0 +1,18 @@
+namespace NetFlix.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnPrice : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Series", "Price", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Series", "Price");
+        }
+    }
+}
